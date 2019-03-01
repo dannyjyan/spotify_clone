dd104b9b674bc2e9f72a93252b716d8c24329fe3
danny yan <danny.yan@gmail.com>
2019-03-01 12:16:50 -0800

    fixed top navbar

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,42 +4,59 @@ class Splash extends React.Component{
 
     render(){
        return ( 
-        <div className="splash-container">
-            <div className="splash-navbar-header">
-                <nav className="splash-navbar"> 
-                    <span className="splash-logo">
-                        Chillify
-                    </span>
-                    <ul className="splash-navbar-right">
-                        <li>Premium</li>
-                        <li>Help</li>
-                        <li>Download</li>
-                        <li className="nav-divider">|</li>
-                        <li><Link to="/signup" id="splash-signup">Sign Up</Link></li>
-                        <li><Link to="/login" id="splash-login">Log In</Link></li>
+        <>
+            <div className="splash-container">
 
-                    </ul>
-                </nav>
+                <div className="splash-navbar-header">
+                    <nav className="splash-navbar"> 
+                        <Link to="/" className="splash-logo">
+                            <div className="spotify-logo"></div>
+                            Chillify
+                        </Link>
+                        <ul className="splash-navbar-right">
+                            <li>Premium</li>
+                            <li>Help</li>
+                            <li>Download</li>
+                            <li className="nav-divider">|</li>
+                            <li><Link to="/signup" id="splash-signup">Sign Up</Link></li>
+                            <li><Link to="/login" id="splash-login">Log In</Link></li>
+
+                        </ul>
+                    </nav>
+                </div>
+                <section>
+                    <div className="splash-main">
+                        <div className="splash-background"> </div>
+                        <h1>
+                            Music for everyone.
+                        </h1>
+                        <h4>
+                            Millions of songs. No credit card needed.
+                        </h4>
+                        <Link to="/signup" className="signup-button">
+                            GET SPOTIFY FREE
+                        </Link>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </section>
             </div>
-            <div className="splash-background"></div>
-            <section>
-                <div className="splash-main">
-                    <h1>
-                        Music for everyone.
-                    </h1>
-                    <h4>
-                        Millions of songs. No credit card needed.
-                    </h4>
-                    <Link to="/signup">
-                    GET SPOTIFY FREE
+            <footer className="footer">
+                <div className="footer-container">
+                    <Link to="/" className="splash-logo">
+                            <div className="spotify-logo"></div>
+                            Chillify
                     </Link>
-
+                    <ul className="footer-links">
+                        <li>Link1</li>
+                        <li>Link2</li>
+                        <li>Link3</li>
+                    </ul>
                 </div>
-                <div>
-
-                </div>
-            </section>
-        </div>
+            </footer>
+        </>
        )}
 }
 

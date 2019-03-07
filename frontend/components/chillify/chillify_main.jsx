a90479108acd73915f11901396ca8cc4cd1ccf31
danny yan <danny.yan@gmail.com>
2019-03-06 16:52:53 -0800

    created main page

--- frontend/components/chillify/chillify_main.jsx
+++ frontend/components/chillify/chillify_main.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {PlaylistIndexItem} from '../playlist/playlist_index_item';
 
 export const ChillifyMain = () => {
 
     return (
-        <div className="main-view"></div>
+        <div className="main-view">
+            <div className="main-view-container">
+                <section className="contentSpacing"> 
+                    <nav className="main-content-nav">
+                        <ul className="main-content-nav-list">
+                            <li className="mc-nav-list-elements">
+                                <Link to="/" className="mc-nav-list-links">FEATURED</Link>
+                            </li>
+                            <li className="mc-nav-list-elements">
+                                <Link to="/" className="mc-nav-list-links">PODCASTS</Link>
+                            </li>
+                            <li className="mc-nav-list-elements">
+                                <Link to="/" className="mc-nav-list-links">CHARTS</Link>
+                            </li>
+                            <li className="mc-nav-list-elements">
+                                <Link to="/" className="mc-nav-list-links">GENRES</Link>
+                            </li>
+                            <li className="mc-nav-list-elements">
+                                <Link to="/" className="mc-nav-list-links">NEW RELEASES</Link>
+                            </li>
+                            <li className="mc-nav-list-elements">
+                                <Link to="/" className="mc-nav-list-links">DISCOVER</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div>
+                        <section className="content">
+                            <div>
+                                <div className="grid-header">
+                                    <h1 className="grid-header-text">Made for CURRENT USER</h1>
+                                </div>
+                                <div className="grid-container-fluid">
+                                    <div className="grid-container-playlist">
+                                        <PlaylistIndexItem />
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </section>
+            </div>
+        </div>
     )
 }
---
+++ frontend/components/playlist/playlist_index_item.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export const PlaylistIndexItem = () => {
+
+    return(
+        <h2>PLAYLIST NAME</h2>
+    )
+}
+
+
+

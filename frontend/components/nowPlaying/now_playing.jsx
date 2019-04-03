9f23206e9b26826d36b0452d7fe88655c7fdb2e9
danny yan <danny.yan@gmail.com>
2019-04-02 19:18:56 -0700

    volume sliders

--- frontend/components/nowPlaying/now_playing.jsx
+++ frontend/components/nowPlaying/now_playing.jsx
@@ -6,16 +6,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 class NowPlaying extends React.Component{
     constructor(props){
         super(props)
-        console.log(props)
+        // console.log(props)        
         this.state = {
             playing: false,
             currentSeconds: 0,
             percentage: 0,
             volume: 0.5,
-            muted: false
+            muted: false,
+            volumeHover: false,
+            progressHover: false
         }
+        this.defaultColor = '#b3b3b3';
+        this.hoverColor = '#1db954';
         this.onDuration = this.onDuration.bind(this);
         this.onProgress = this.onProgress.bind(this);
+        this.toggleVolumeHover = this.toggleVolumeHover.bind(this);
+        this.toggleProgressHover = this.toggleProgressHover.bind(this);
+        this.toggleMute = this.toggleMute.bind(this);
         this.toggleMute = this.toggleMute.bind(this);
         this.togglePlay = this.togglePlay.bind(this);
         this.updateTime = this.updateTime.bind(this);
@@ -32,6 +39,14 @@ class NowPlaying extends React.Component{
             percentage: progress.played
         })
     }
+
+    toggleVolumeHover(){
+        this.setState({volumeHover: !this.state.volumeHover})
+    }
+    toggleProgressHover(){
+        this.setState({progressHover: !this.state.progressHover})
+    }
+    
 
     toggleMute(){
         this.setState({
@@ -55,8 +70,14 @@ class NowPlaying extends React.Component{
         
         let volume = (1 - this.state.volume);
         if (this.state.muted) volume = 0;
-
-        
+        let progressColor = {backgroundColor: this.defaultColor};
+        if (this.state.progressHover) {
+            progressColor = {backgroundColor: this.hoverColor};
+        } 
+        let volumeColor = {backgroundColor: this.defaultColor};
+        if (this.state.progressHover) {
+            volumeColor = {backgroundColor: this.hoverColor};
+        } 
 
         return (
         <footer className="now-playing-bar-container">
@@ -73,22 +94,33 @@ class NowPlaying extends React.Component{
                         <div className="np-controls-buttons">
                             <div className="control-button">
                             </div>
-                            <div className="control-button control-button-circled play-button icon-play" onClick={this.togglePlay}>
-                                <FontAwesomeIcon className="icon" icon="play" />    
-                            </div>
-                            <div className="control-button control-button-circled play-button icon-pause" onClick={this.togglePlay}>
-                                <FontAwesomeIcon className="icon" icon="pause" />    
-                            </div>
-
+                            {this.state.playing ? 
+                                <div className="control-button control-button-circled play-button icon-pause" onClick={this.togglePlay}>
+                                    <FontAwesomeIcon className="icon" icon="pause" />    
+                                </div>
+                                :
+                                <div className="control-button control-button-circled play-button icon-play " onClick={this.togglePlay} >
+                                    <FontAwesomeIcon className="icon" icon="play" />    
+                                </div>
+                            }   
                             <div className="control-button"></div>
                         </div>
                         <div className="playback-bar">
                             <div className="progress-current-time">{this.state.currentSeconds}</div>
                             <div className="progress-bar">
-                                <div className="progress-bar-bg">
-                                    <div className="progress-bar-wrapper">
-                                        <div className="progress-bar-moving" style={{'transform': 'translateX(-'+ (100 - this.state.percentage*100)+'%)'}}>
+                                <div className="progress-bar-bg" onMouseEnter={this.toggleProgressHover} onMouseLeave={this.toggleProgressHover}>
+                                    <div className="progress-bar-wrapper" > 
+                                        <div className="progress-bar-moving" style={Object.assign({}, {'transform': 'translateX(-'+ (100 - this.state.percentage*100)+'%)'}, progressColor)} >
+
                                         </div>
+                                        <div className="progress-bar-button">
+                                        </div>
+
+                                        {/* {this.state.playing ? */}
+                                            {/* // <div className="progress-bar-button-wrapper">
+                                            // </div>  */}
+                                            {/* : "" */}
+                                        {/* } */}
                                     </div>
                                 </div>
                             </div>
@@ -110,19 +142,22 @@ class NowPlaying extends React.Component{
                 </div>
                 <div className="now-playing-bar-right">
                     <div className="volume-controls">
-                        <div className="control-button play-button icon-volume-up" onClick={this.toggleMute}>
-                            <FontAwesomeIcon className="icon" icon="volume-up" />    
-                        </div>
-                        <div className="control-button play-button icon-volume-mute" onClick={this.toggleMute}>
-                            <FontAwesomeIcon className="icon" icon="volume-mute" />    
-                        </div>
-
+                        {this.state.muted ?  
+                            <div className="control-button play-button icon-volume-mute" onClick={this.toggleMute}>
+                                <FontAwesomeIcon className="icon" icon="volume-mute" />    
+                            </div>
+                            :
+                            <div className="control-button play-button icon-volume-up" onClick={this.toggleMute}>
+                                <FontAwesomeIcon className="icon" icon="volume-up" />    
+                            </div>
+                        }
 
 
                         <div className="progress-bar">
-                            <div className="progress-bar-bg">
+                            <div className="progress-bar-bg" onMouseEnter={this.toggleVolumeHover} onMouseLeave={this.toggleVolumeHover}>
                                 <div className="progress-bar-wrapper">
-                                    <div className="progress-bar-moving" style={{'transform': 'translateX(-'+ (100-volume*100) +'%)'}} > </div>
+                                    <div className="progress-bar-moving" style={ Object.assign({}, {'transform': 'translateX(-'+ (100-volume*100) +'%)' }, volumeColor)} > 
+                                    </div>
 
                                 </div>
                             </div>
